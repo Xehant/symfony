c8b0ad354785640ea0864983f0871b1f423f40ee
Owen <80330087+Xehant@users.noreply.github.com>
2022-12-12 17:08:54 +0100

    calculator sur index

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -2,22 +2,27 @@
 
 namespace App\Controller;
 
+use App\Taxes\Calculator;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HelloController
 {
-    public function __construct(LoggerInterface $logger)
+    protected $calculator;
+    public function __construct(Calculator $calculator)
     {
-        $this->logger = $logger;
+        $this->calculator =$calculator;
     }
-
-
-
-    public function hello($prenom = "World")
+    /** 
+    *  @Route("/hello/{prenom?World}", name="hello");
+    */
+    public function hello($prenom = "World", LoggerInterface $logger)
     {
-        
+        $logger->error("Mon log");
+
+        $tva= $this->calculator->calcul(100);
+        dump($tva);
         return new Response("Hello $prenom");
     }
-}
+} 
--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -2,22 +2,27 @@
 
 namespace App\Controller;
 
+use App\Taxes\Calculator;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class TestController
 {
+    protected $calculator;
+    public function __construct(Calculator $calculator)
+    {
+        $tva=$this->calculator =$calculator;
+    }
+
     public function index()
     {
-        var_dump("Ca fonctionne");
-        die();
+        $tva =$this->calculator->calcul(100);
+        dump($tva);
+        dd("Ca fonctionne");
+        
     }
     public function test(Request $request,$age)
     {
-
-      
-
-
     return new Response("vous avez $age ans");
        
     }
